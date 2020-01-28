947775a453d222f923fb7fef5ecdff9538ae7cf3
root <root@centos.local>
2020-01-28 20:40:48 -0300

    added btrfs_files_errors to list all files that contains errors in a BTRFS filesystem. Run after a scrub!

--- bin/centos/ha-watchdog.sh
+++ bin/centos/ha-watchdog.sh
@@ -8,7 +8,9 @@ for id in $errorOnes ; do
 	ha-manager set $id -state disabled
 done
 sleep 5
+errorOnes=$(ha-manager status  | grep disable | awk '{print $2}' | awk -F':' '{print $2}')
 for id in $errorOnes ; do
+	echo "starting $id"
 	ha-manager set $id -state started
 done
 ha-manager status
