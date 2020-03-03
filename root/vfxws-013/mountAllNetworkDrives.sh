7c43a47ae1053a4538000de62953e6bf2f7c53df
root <root@vfxws-013.local>
2020-03-02 21:40:51 -0300

    now we check if the mounted drive is listing anything. If not, umount it forcefully and try to mount again!

--- root/vfxws-013/mountAllNetworkDrives.sh
+++ root/vfxws-013/mountAllNetworkDrives.sh
@@ -2,8 +2,11 @@
 
 # mount fuse and nfs mounts in fstab
 for n in $(egrep 'fuse|nfs' /etc/fstab | awk '{print $2}') ; do 
-        if [ "$(mount | grep -v autofs | grep $n)" == "" ] ; then
+        if [ "$(mount | grep -v autofs | grep $n)" == "" ] || [ "$(timeout 30 ls $n)" == "" ] ; then
                 mkdir -p $n
+		umount -f -l $n || true
+		umount -f -l $n || true
+		umount -f -l $n || true
                 mount $n
 	fi
 done
