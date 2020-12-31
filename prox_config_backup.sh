2dd80490865c9b827c042e4f91604a01a9268db5
root <root@pve-manjaro.local>
2020-12-31 18:59:43 -0200

    avoid deleting files that are not .tar.gz in the backup folder

--- prox_config_backup.sh
+++ prox_config_backup.sh
@@ -80,8 +80,8 @@ function are-we-root-abort-if-not {
 }
 
 function check-num-backups {
-    if [[ $(ls ${_bdir} -l | grep ^- | wc -l) -ge $MAX_BACKUPS ]]; then
-      local oldbackup="$(ls ${_bdir} -t | tail -1)"
+    if [[ $(ls ${_bdir}/*.tar.gz -l | grep ^- | wc -l) -ge $MAX_BACKUPS ]]; then
+      local oldbackup="$(ls ${_bdir}/*.tar.gz -t | tail -1)"
       echo "${_bdir}/${oldbackup}"
       rm "${_bdir}/${oldbackup}"
     fi
